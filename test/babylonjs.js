fbe27d088b58041016755071daa44a7117c88e05
Даньков Олександр <dankov.kiev@gmail.com>
2024-01-26 03:56:35 +0200

    added puppeteer

--- test/babylonjs.js
+++ test/babylonjs.js
@@ -1,55 +1,93 @@
 const fs = require('fs');
 const BABYLON = require('babylonjs');
 const {createCanvas} = require('../lib');
+const path = require('path');
+const {URL} = require('url');
+const {Blob} = require('buffer');
 
-// polyfill
-global.HTMLElement = function () {};
+global.HTMLElement = function () {
+};
 global.window = {
   setTimeout,
-  addEventListener() {},
+  addEventListener() {
+  },
 };
 global.navigator = {};
 global.document = {
   createElement() {
     return createCanvas(300, 150);
   },
-  addEventListener() {},
+  addEventListener() {
+  },
 };
 
 // Get the canvas DOM element
-const canvas = createCanvas(512, 512);
+const canvas = createCanvas(1920, 1080);
 
 // Load the 3D engine
 const engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
 
 // CreateScene function that creates and return the scene
-const createScene = function () {
+const createScene = async function () {
   // Create a basic BJS Scene object
   const scene = new BABYLON.Scene(engine);
+  // GLTF LOADER
+  const modelBlob = fs.readFileSync(path.resolve(__dirname, 'assets/model.glb'), 'binary');
+    const File = import('file-api').then((module) => module.File);
+    const file = new File(modelBlob, 'model.glb', {type: 'application/octet-stream'});
+
+    // const blob = new Blob([modelBlob], {type: 'application/octet-stream'});
+  // but for node env where Blob is not available
+
+    const buffer = Buffer.from(modelBlob, 'binary');
+    //get the blob in node env without new Blob
+    const blob = new Blob([buffer], {type: 'application/octet-stream', endings: 'native', href: 'model.glb', filename: 'model.glb'});
+
+    const str = blob.toString();
+
+
+    //get the blob from buffer in node env
+
+
+  // const url = URL.createObjectURL(str);
+  const promise = await BABYLON.SceneLoader.LoadAssetContainerAsync(file, '', scene, null, null, null, '.glb').then((container) => {
+    container.addAllToScene();
+  });
+
+    // Once the scene is loaded, just register a render loop to render it
+
+
   // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-  const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
-  // Target the camera to scene origin
-  camera.setTarget(BABYLON.Vector3.Zero());
-  // Attach the camera to the canvas
-  camera.attachControl(canvas, false);
-  // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
-  const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-  // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-  const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
-  // Move the sphere upward 1/2 of its height
-  sphere.position.y = 1;
+  // const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+  // // Target the camera to scene origin
+  // camera.setTarget(BABYLON.Vector3.Zero());
+  // // Attach the camera to the canvas
+  // camera.attachControl(canvas, false);
+  // // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
+  // const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+  // // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
+  // const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
+  // // Move the sphere upward 1/2 of its height
+  // sphere.position.y = 1;
   // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-  const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+  // const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
   // Return the created scene
-  return scene;
+    return promise;
 };
 
 // call the createScene function
-const scene = createScene();
+const runApp = async () => {
+  const scene = await createScene();
+  scene.render();
 
-scene.render();
+  fs.writeFileSync('./snapshot/snap-babylon.png', canvas.toBuffer());
+}
 
-fs.writeFileSync('./snapshot/snap-babylon.png', canvas.toBuffer());
+runApp();
+
+
+
+
 // run the render loop
 // engine.runRenderLoop(() => {
 //   scene.render();
--- test/threejs.js
+++ test/threejs.js
@@ -1,46 +1,141 @@
-const fs = require('fs');
-const THREE = require('three');
-const GIFEncoder = require('gifencoder');
-const {createCanvas} = require('../lib');
+// const fs = require('fs');
+// const {JSDOM} = require('jsdom');
+// const gl = require('gl');
+// const THREE = require('three');
+// const GIFEncoder = require('gifencoder');
+//
+// const { GLTFLoader } = require('three/examples/jsm/loaders/GLTFLoader');
+// rewrited to imports
 
-const width = 512,
-  height = 512;
+import fs from 'fs';
+// import {JSDOM} from 'jsdom';
+// import gl from 'gl';
+import * as THREE from 'three';
+import GIFEncoder from 'gifencoder';
+import puppeteer from 'puppeteer';
+// import {GLTFLoader} from "three/addons";
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+const modelBuffer = fs.readFileSync('./assets/model.glb');
+const modelBase64 = modelBuffer.toString('base64');
+import gltfPipeline from 'gltf-pipeline';
+const glbToGltf = gltfPipeline.glbToGltf;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
-const canvas = createCanvas(width, height);
 
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-});
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+(async () => {
+  const glbToGltfResult = await glbToGltf(modelBuffer);
+  const modelJson = JSON.stringify(glbToGltfResult.gltf);
+    const browser = await puppeteer.launch({headless: 'new'});
+//html with canvas to render in browser
+    const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <title>Title</title>
+   
+</head>
+<body>
+<div id="container">
+    <canvas id="canvas"></canvas>
+    <script async src="https://esm.sh/three" type="module"></script>
+    <script async src="https://esm.sh/three@0.160.1/examples/jsm/loaders/GLTFLoader" type="module"></script>
+<!--    gif maker-->
+    <script defer type="module">
+        import * as THREE from 'https://esm.sh/three';
+        import {GLTFLoader} from 'https://esm.sh/three/examples/jsm/loaders/GLTFLoader';
+        window.THREE = THREE;
+        window.GLTFLoader = GLTFLoader;
+        const canvas = document.getElementById('canvas');
+        canvas.width = 1920;
+        canvas.height = 1080;
+       
+        const gl = canvas.getContext('webgl2');
+        const renderer = new THREE.WebGLRenderer({canvas: canvas, context: gl});
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, 1920 / 1080, 0.1, 1000);
+        camera.position.z = 5;
+        const loader = new GLTFLoader();
+        const gltfString = '${modelJson}';
+        window.gltfString = gltfString;
+        
+        const gltf = JSON.parse(gltfString);
+        (async () => { 
+            const gltf = await loader.parseAsync(gltfString, '');
+           
+            const model = gltf.scene;
+            scene.add(model);
+            let frame = 0;
+            
+            const animate = () => {
+                if (frame < 300) {
+                model.rotation.x += 0.01;
+                renderer.render(scene, camera);
+                frame++;
+               } else {
+                window.rendered = true;
+               }
+                requestAnimationFrame(animate);
+            
+            };
+           
+              animate();
+            
+        })();
+       
+      
+       
+        
+       
+    </script>
+</div>
+</body>
+</html>
+`;
+//create page
+    const page = await browser.newPage();
+//set viewport
+    await page.setViewport({width: 1920, height: 1080});
+//set content
+    await page.setContent(html);
 
-camera.position.z = 5;
+//log all console messages from page
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    page.on('error', err => console.log('PAGE LOG:', err));
+    page.on('pageerror', err => console.log('PAGE LOG:', err));
+    page.on('requestfailed', err => console.log('PAGE LOG:', err));
+    page.on('domcontentloaded', () => console.log('PAGE LOG:', 'domcontentloaded'));
+    page.on('load', () => console.log('PAGE LOG:', 'load'));
+    page.on('response', () => console.log('PAGE LOG:', 'response'));
+    page.on('request', () => console.log('PAGE LOG:', 'request'));
+    console.log('PAGE LOG:', 'start');
+//wait for page to load
 
-const encoder = new GIFEncoder(width, height);
-encoder.createReadStream().pipe(fs.createWriteStream('./snapshot/threejs-cube.gif'));
-encoder.start();
-encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
-encoder.setDelay(16); // frame delay in ms
-encoder.setQuality(10); // image quality. 10 is default.
+    console.log('PAGE LOG:', 'loaded');
+    const threejs = await page.evaluateHandle(() => window.THREE).then((res) => res.jsonValue());
+    const loader = await page.evaluateHandle(() => {
+        return {
+            rendered: window.rendered
+        }
+    }).then((res) => res.jsonValue());
+    console.log('PAGE LOG:', loader);
 
-let idx = 0;
-function update() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-  if(idx > 0) {
-    encoder.addFrame(canvas.__ctx__);
-    console.log(`add frame ${idx}`);
-  }
-  idx++;
-  if(idx < 300) {
-    setTimeout(update, 16);
-  }
-}
-update();
+
+//wait for model to load
+//     await page.waitForFunction('window.rendered === true');
+
+//get canvas element
+    const canvas = await page.$('#canvas');
+//get canvas content as base64
+    const base64 = await canvas.screenshot({encoding: 'base64'});
+//create buffer from base64
+    const buffer = Buffer.from(base64, 'base64');
+//save buffer to file
+    fs.writeFileSync('./assets/screenshot.png', buffer);
+//close browser
+    await browser.close();
+
+
+})();
