7c078a8cc23075066fcf453bad4591a4563af1eb
Даньков Олександр <dankov.kiev@gmail.com>
2024-01-26 04:37:32 +0200

    advanced browsing

--- test/threejs.js
+++ test/threejs.js
@@ -1,31 +1,20 @@
-// const fs = require('fs');
-// const {JSDOM} = require('jsdom');
-// const gl = require('gl');
-// const THREE = require('three');
-// const GIFEncoder = require('gifencoder');
-//
-// const { GLTFLoader } = require('three/examples/jsm/loaders/GLTFLoader');
-// rewrited to imports
 
 import fs from 'fs';
-// import {JSDOM} from 'jsdom';
-// import gl from 'gl';
-import * as THREE from 'three';
+
+
 import GIFEncoder from 'gifencoder';
 import puppeteer from 'puppeteer';
-// import {GLTFLoader} from "three/addons";
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 const modelBuffer = fs.readFileSync('./assets/model.glb');
-const modelBase64 = modelBuffer.toString('base64');
+
 import gltfPipeline from 'gltf-pipeline';
+
 const glbToGltf = gltfPipeline.glbToGltf;
 
 
-
-
 (async () => {
-  const glbToGltfResult = await glbToGltf(modelBuffer);
-  const modelJson = JSON.stringify(glbToGltfResult.gltf);
+    const glbToGltfResult = await glbToGltf(modelBuffer);
+    const modelJson = JSON.stringify(glbToGltfResult.gltf);
     const browser = await puppeteer.launch({headless: 'new'});
 //html with canvas to render in browser
     const html = `
@@ -53,42 +42,50 @@ const glbToGltf = gltfPipeline.glbToGltf;
         canvas.height = 1080;
        
         const gl = canvas.getContext('webgl2');
-        const renderer = new THREE.WebGLRenderer({canvas: canvas, context: gl});
+        const renderer = new THREE.WebGLRenderer({canvas: canvas, context: gl, dpi: 2});
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, 1920 / 1080, 0.1, 1000);
-        camera.position.z = 5;
+        const light = new THREE.AmbientLight(0xffffff, 1);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(0, 0, 2);
+        scene.add(directionalLight);
+        scene.add(light);
+        const envMap = new THREE.CubeTextureLoader().load([
+            'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-x.jpg',
+            'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-x.jpg',
+            'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-y.jpg',
+            'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-y.jpg',
+            'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-z.jpg',
+            'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-z.jpg',
+        ]);
+        scene.background = envMap;
+        
+        
+        camera.position.z = 2;
+        camera.position.y = 0;
+        camera.position.x = 0;
+        camera.lookAt(0, 0, 0);
+        
         const loader = new GLTFLoader();
         const gltfString = '${modelJson}';
         window.gltfString = gltfString;
         
         const gltf = JSON.parse(gltfString);
         (async () => { 
+            
             const gltf = await loader.parseAsync(gltfString, '');
            
             const model = gltf.scene;
             scene.add(model);
-            let frame = 0;
-            
-            const animate = () => {
-                if (frame < 300) {
-                model.rotation.x += 0.01;
+            //rotate model
+            const render = () => {
+                requestAnimationFrame(render);
+                model.rotation.y += 0.01;
                 renderer.render(scene, camera);
-                frame++;
-               } else {
-                window.rendered = true;
-               }
-                requestAnimationFrame(animate);
-            
             };
-           
-              animate();
-            
+            render();
         })();
-       
-      
-       
-        
-       
+
     </script>
 </div>
 </body>
@@ -117,25 +114,53 @@ const glbToGltf = gltfPipeline.glbToGltf;
     const threejs = await page.evaluateHandle(() => window.THREE).then((res) => res.jsonValue());
     const loader = await page.evaluateHandle(() => {
         return {
-            rendered: window.rendered
+            rendered: window.rendered,
+            base64Video: window.base64Video,
         }
     }).then((res) => res.jsonValue());
-    console.log('PAGE LOG:', loader);
+    console.log(loader);
+    const base64Video = loader.base64Video;
 
 
-//wait for model to load
-//     await page.waitForFunction('window.rendered === true');
 
-//get canvas element
     const canvas = await page.$('#canvas');
-//get canvas content as base64
-    const base64 = await canvas.screenshot({encoding: 'base64'});
-//create buffer from base64
-    const buffer = Buffer.from(base64, 'base64');
-//save buffer to file
-    fs.writeFileSync('./assets/screenshot.png', buffer);
-//close browser
-    await browser.close();
 
 
-})();
+
+    const encoder = new GIFEncoder(1920, 1080);
+    encoder.setRepeat(0);
+
+    const gifStream = encoder.createReadStream();
+    gifStream.pipe(fs.createWriteStream('./assets/model-new.gif'));
+
+
+    let frame = 0
+    const maxFrames = 100;
+    encoder.start();
+
+    const render = async () => {
+
+        return new Promise(async (resolve, reject) => {
+
+
+                if (frame < maxFrames) {
+                    frame++;
+                    const base64 = await canvas.screenshot({encoding: 'base64', type: 'webp', quality: 100});
+                    fs.writeFileSync(`./assets/${frame}.webp`, base64, 'base64');
+
+
+                    setTimeout(render, 1000 / 60);
+                } if (frame === maxFrames) {
+                    encoder.finish();
+                    resolve();
+                }
+            }
+        );
+
+        } ;
+        await render();
+        await browser.close();
+    } )();
+
+
+
